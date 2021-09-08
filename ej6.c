74e93504bc0c6dac0cd31eb7fd33a271f2a37cd5
ashandme <fufbisumy@gmail.com>
2021-09-08 20:22:46 -0300

    @

--- ej6.c
+++ ej6.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-float ovtaba(int x){
+float octaba(int x){
     return (float)x/8;
 }
 int main(){
     int a;
     printf("ingrese un numero entero: ");
     scanf("%d", &a);
-    printf("la ovtaba parte es %f", ovtaba(a));
+    printf("la ovtaba parte es %f", octaba(a));
 }
