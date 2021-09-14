a34bf0dc137662cb67dc7367799e241169a8f745
SantiagoC22 <86362788+SantiagoC22@users.noreply.github.com>
2021-09-14 10:28:02 -0300

    Update ej1.c

--- ej1.c
+++ ej1.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
-int escanear(){
-  int a;
-  printf("ingrese un numero: ");
-  scanf("%d", &a);
-  return a;
-}
-int main(){
-    int a,b;
-    a = escanear();
-    b = escanear();
-    printf("%d\n%d\n", a, b);
-}
+
